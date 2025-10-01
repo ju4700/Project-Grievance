42c84e7543cc6892fbfbae288cb3764e528bae2c
ju4700 <jalal.dev.design@gmail.com>
2025-10-01 15:21:57 +0600

    feat: initialize grievance project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite and React logos as SVG assets.
    - Created Navbar component for the application.
    - Set up Tailwind CSS in index.css.
    - Configured main entry point (main.tsx) to render Navbar.
    - Established TypeScript configuration files for app and node environments.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ grievance/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <div className="flex justify-center">
+            <div className="w-[1200px] h-16 bg-white rounded-bl-[10px] rounded-br-[10px] shadow-[0px_3px_22px_2px_rgba(0,55,22,0.20)] flex items-center justify-end">
+                <div className="w-24 h-9 bg-green-950 rounded-lg text-center flex items-center justify-center mr-14">
+                    <div className="text-white text-base font-bold font-['Inter']">Register</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
